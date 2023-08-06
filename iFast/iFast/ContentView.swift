07b0e95fee1388ab39c49e353c44f8085a49ea06
Austin Potts <austinpotts@Andariels-Air.lan>
2023-08-06 15:37:48 -0400

    Completed configuration of the rest of the UI elements

--- iFast/iFast/ContentView.swift
+++ iFast/iFast/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            //Mark: BAckground
+            //MARK: BAckground
             Color(#colorLiteral(red: 0.03348137066, green: 0.0009218981722, blue: 0.0571668148, alpha: 1))
                 .ignoresSafeArea()
             
@@ -19,10 +19,73 @@ struct ContentView: View {
     }
     
     var content: some View {
-        VStack {
-            //MARK: Progress ring
-            ProgressRing()
+        ZStack {
+            VStack(spacing: 40) {
+                //MARK: Title
+                Text("Let's Get Started!")
+                    .font(.headline)
+                    .foregroundColor(Color(#colorLiteral(red: 0.07639981061, green: 0.9389412403, blue: 1, alpha: 1)))
+                
+                //MARK: Fasting Plan
+                Text("16:8")
+                    .fontWeight(.semibold)
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 8)
+                    .background(.thinMaterial)
+                    .cornerRadius(20)
+                
+                Spacer()
+                
+            }
+                
+            VStack(spacing: 40) {
+                //MARK: Progress ring
+                ProgressRing()
+                
+                
+                HStack(spacing: 60) {
+                    //MARK: Start Time
+                    
+                    VStack(spacing: 5) {
+                        Text("Start")
+                            .opacity(0.7)
+                        
+                        Text(Date(), format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                    
+                    //MARK: Start Time
+                    
+                    VStack(spacing: 5) {
+                        Text("End")
+                            .opacity(0.7)
+                        
+                        //16 is for testing
+                        Text(Date().addingTimeInterval(16), format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                }
+                
+                //MARK: Button
+                
+                Button {
+                    
+                } label: {
+                    Text("Start Fasting")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 24)
+                        .padding(.vertical, 8)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                }
+
+            }
+            
+            .padding()
         }
+        .foregroundColor(.white)
+
     }
 }
 
