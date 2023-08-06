c8f1e83ee2be284961219f09bec841ef1d0c323a
Austin Potts <austinpotts@Andariels-Air.lan>
2023-08-06 14:16:05 -0400

    Project Configured

---
+++ iFast/iFast/iFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFastApp.swift
+//  iFast
+//
+//  Created by Austin Potts on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct iFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
